622562339a45a2562d57a0525edc9d6e421d3b20
EmanuelFontalba <emanuelfontalba@gmail.com>
2020-01-21 20:57:25 +0100

    create home page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(".dont").click(function(e){
+    e.preventDefault();
+    
+    $(".not").removeClass("none");
+    setTimeout(function(){
+        $(".not").addClass("none");
+    }, 2000);
+});
+
 
