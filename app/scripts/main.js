9abcb896cabd34bdf16491312a5b6e92dfc54c7d
EmanuelFontalba <emanuelfontalba@gmail.com>
2020-02-01 14:35:24 +0100

    scroll magic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,19 @@
-$(".dont").click(function(e){
-    e.preventDefault();
-    
-    $(".not").removeClass("none");
-    setTimeout(function(){
-        $(".not").addClass("none");
-    }, 2000);
+$(".dont").click(function(e) {
+  e.preventDefault();
+
+  $(".not").removeClass("none");
+  setTimeout(function() {
+    $(".not").addClass("none");
+  }, 2000);
 });
 
+const controller = new ScrollMagic.Controller();
 
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#trigger1",
+  duration: 10000,
+  tweenChanges: true
+})
+  .setTween("#animate-zoom", { scale: 200, translateX: "-100rem" })
+  .setPin("#animate-zoom")
+  .addTo(controller);
